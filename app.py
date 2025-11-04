d671220fb5a795d41c8ac3d90aea99bf958f61cd
Mangting Lee <mangtinglee@MonMacBook-Air.local>
2025-11-04 16:45:28 +0800

    feat: add file unzip

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, Form
+import zipfile
+import io
+
 
 app = FastAPI()
 
@@ -11,19 +14,28 @@ def hello():
 @app.post("/analyze")
 async def analyze_code(
     problem_description: str = Form(...),
-    code_zip: UploadFile = File(...)
+    code_zip: UploadFile = File(...) 
+    # UploadFile = File object provided by FastAPI. 
+    # The uploaded file is wrapped as this object.
+
 ):
+    # read the upload content
+    content = await code_zip.read()
+
+    # convert the content into a ZIP object
+    with zipfile.ZipFile(io.BytesIO(content)) as zf:
+        file_list = zf.namelist()
+
     # sample response to confirm data received
     return {
         "status": "received",
         "problem_description": problem_description,
         "filename": code_zip.filename,
-        "file_size": code_zip.size
+        "file_size": code_zip.size,
+        "file_in_zip": file_list  
     }
 
 if __name__ == "__main__":
     import uvicorn
     # run the app locally
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
